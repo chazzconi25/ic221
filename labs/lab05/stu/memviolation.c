898eddcb7c3259a18be86d54454aa75c9040db6d
Charlie Francesconi <m251806@usna.edu>
2023-02-16 20:52:57 -0500

    random stuff

--- labs/lab05/stu/memviolation.c
+++ labs/lab05/stu/memviolation.c
@@ -10,14 +10,12 @@ int main(int argc, char * argv[]){
   //simple copy routine
   char * str = malloc(strlen(hello));
   for(i = 0; i < strlen(hello); i ++){
-    printf("%d",i);
     str[i] = hello[i];
-    printf(" %c\n", str[i]);
   }
+  str[strlen(hello)-1] = '\0';
   
 
   printf("%s\n", str);
-printf("%s\n", "test");
   free(str);
   return 0;
 }
