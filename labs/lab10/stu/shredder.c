668721f3ba7c40b252d821ef5d25abad9a57a5b3
chazzconi25 <m251806@usna.edu>
2023-04-17 15:07:35 -0400

    hw10

--- labs/lab10/stu/shredder.c
+++ labs/lab10/stu/shredder.c
@@ -16,42 +16,40 @@ pid_t cpid;
 
 void tick(int signum) {
   //TODO: complete the SIGALRM handler
-  //
   // On each alarm, print
-  //    printf("%d: tick-tock\n", ticks);
-  //
+  printf("%d: tick-tock\n", ++ticks);
   // If 5 ticks occur, kill the child with SIGKILL
-  //
+  if(ticks ==  5) {
+    kill(cpid, SIGKILL);
+    return;
+  }
   // (hint: don't forget to set the next alarm)
+  alarm(1);
 }
 
 int main(int argc, char * argv[]) {
 
   if ((cpid = fork()) == 0) {
     /*CHILD*/
-
     execvp(argv[1], argv+1);
     perror(argv[1]);
     _exit(1);
   }
   else if (cpid > 0) {
     /*PARENT*/
-
     signal(SIGALRM, tick);
+    alarm(1);
 
     //TODO: Complete the program
-    //
+    wait(NULL);
     // If the child finished before 3 ticks:
-    //  "Blast that grotesque ganglion! You let them get away!\n"
-    //
-    // If the child finished after 3 ticks but not killed:
-    //  "Sayonara you shell-backed simpletons. I'll get you next time!\n"
-    //
-    // If the child was killed:
-    //  "Tonight I dine on turtle soup! Muhaha!\n"
-    //
-    // (hint: don't forget to turn off unscheduled alarms)
-    // (hint: recall the WIF---() functions ...)
+    if(ticks < 3) {// If the child finished after 3 ticks but not killed:
+      printf("Blast that grotesque ganglion! You let them get away!\n");
+    } else if(ticks >= 3 && ticks < 5) {
+      printf("Sayonara you shell-backed simpletons. I'll get you next time!\n");
+    } else {// If the child was killed:
+      printf("Tonight I dine on turtle soup! Muhaha!\n");
+    }
 
   }
   else {
